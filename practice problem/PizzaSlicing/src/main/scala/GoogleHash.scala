2cd79d56944c9e263454cb4f53c41f8f4f7dbde2
Radim Pavlicek <radim.pavlicek@gmail.com>
2017-01-12 15:13:39 +0100

    update

--- practice problem/PizzaSlicing/src/main/scala/GoogleHash.scala
+++ practice problem/PizzaSlicing/src/main/scala/GoogleHash.scala
@@ -6,6 +6,7 @@ import scala.io.Source
 
 object GoogleHash {
 
+
   def main(args: Array[String]): Unit = {
     //        val filename = "logo"
     val filename = "example"
@@ -15,8 +16,13 @@ object GoogleHash {
     val data = tmp.head.split(' ')
     val rows = Integer.parseInt(data(0))
     val cols = Integer.parseInt(data(1))
+    val min = Integer.parseInt(data(2))
+    val max = Integer.parseInt(data(3))
 
     println("Rows " + rows + " Cols: " + cols)
+
+
+
 //    var i = 0
 //    val commands = tmp.tail.zipWithIndex.flatMap {
 //      case (line, index) => p1(index, line)
