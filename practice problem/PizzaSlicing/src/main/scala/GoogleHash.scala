3e3b358d3a741f9640256700693df93c8fa74afa
Nikolay Kushin <zikolach@Nikolays-MacBook-Pro-2.local>
2017-02-19 17:31:45 +0100

    prepare

--- practice problem/PizzaSlicing/src/main/scala/GoogleHash.scala
+++ practice problem/PizzaSlicing/src/main/scala/GoogleHash.scala
@@ -1,36 +1,49 @@
 import scala.io.Source
+import scala.language.postfixOps
 
 object GoogleHash {
 
   def main(args: Array[String]): Unit = {
+  }
+
+  case class Pos(r: Int, c: Int) {
+    def inside(r1: Int, c1: Int, r2: Int, c2: Int): Boolean = r >= r1 && r <= r2 && c >= c1 && c <= c2
+  }
+
+  case class Pizza(cells: Seq[Seq[Char]]) {
+    val ingredients: Map[Char, Set[Pos]] = cells.indices
+      .flatMap(row => cells(row).indices
+        .map(col => (cells(row)(col), Pos(row, col))))
+      .groupBy(_._1)
+      .map({ case (key, value) => key -> value.map(_._2).toSet })
+
 
   }
 
-  case class Task(pizza: Seq[Seq[Char]], min: Int, max: Int)
+  case class Task(pizza: Pizza, min: Int, max: Int, slices: Set[Slice] = Set.empty) {
+    def slice(): Task = ???
 
-  case class Slice(r1: Int, c1: Int, r2: Int, c2: Int) {
-    override def toString = s"$r1 $c1 $r2 $c2"
+    override def toString: String =
+      s"""${slices.size}
+         |${slices.map(s => s"${s.r1} ${s.c1} ${s.r2} ${s.c2}").mkString("\n")}
+         |""".stripMargin
   }
 
-  def slicePizza(filename: String): String = {
-    val Task(pizza, min, max) = readPizza(filename)
-
-    val slices = ""
-
-    slices
+  object Task {
+    def apply(filename: String): Task = {
+      val header :: rows = Source.fromFile(filename).getLines().toList
+      val rc :: cc :: min :: max :: Nil = header.split(' ').map(_.toInt).toList
+      val data = rows.map(_.toCharArray.toSeq)
+      assert(rc == rows.length)
+      assert(data.forall(_.length == cc))
+      Task(Pizza(data), min, max)
+    }
   }
 
-  def readPizza(filename: String): Task = {
-    val header :: rows = Source.fromFile(filename).getLines().toList
-    val rc :: cc :: min :: max :: Nil = header.split(' ').map(_.toInt).toList
-    val data = rows.map(_.toCharArray.toSeq)
-    assert(rc == rows.length)
-    assert(data.forall(_.length == cc))
-    Task(data, min, max)
+  case class Slice(pizza: Pizza, r1: Int, c1: Int, r2: Int, c2: Int) {
+    val ingredients: Map[Char, Set[Pos]] = pizza.ingredients.map { case (ing, positions) =>
+      ing -> positions.filter(_.inside(r1, c1, r2, c2))
+    }
   }
 
-  def saveSlices(slices: Seq[Slice]): String =
-    s"""${slices.size}
-       |${slices.mkString("\n")}
-       |""".stripMargin
 }
--- practice problem/PizzaSlicing/src/test/scala/GoogleHashTest.scala
+++ practice problem/PizzaSlicing/src/test/scala/GoogleHashTest.scala
@@ -1,38 +1,55 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 
-class GoogleHashTest extends FlatSpec with Matchers {
+class GoogleHashTest extends WordSpec with Matchers {
 
   import GoogleHash._
 
-  "Hashcode" should "save slices to file" in {
-    val expected =
-      """3
-        |0 0 2 1
-        |0 2 2 2
-        |0 3 2 4
-        |""".stripMargin
-    val result = saveSlices(Seq(Slice(0, 0, 2, 1), Slice(0, 2, 2, 2), Slice(0, 3, 2, 4)))
-    result should be(expected)
+  private val examplePizza = Pizza(Seq(
+    Seq('T', 'T', 'T', 'T', 'T'),
+    Seq('T', 'M', 'M', 'M', 'T'),
+    Seq('T', 'T', 'T', 'T', 'T')))
+
+
+  "Hashcode" should {
+    "save slices to file" in {
+      val expected =
+        """3
+          |0 0 2 1
+          |0 2 2 2
+          |0 3 2 4
+          |"""
+          .stripMargin
+      val result = Task(examplePizza, 0, 0, Set(
+        Slice(examplePizza, 0, 0, 2, 1),
+        Slice(examplePizza, 0, 2, 2, 2),
+        Slice(examplePizza, 0, 3, 2, 4))).toString
+      result should be(
+        expected)
+    }
+
+    "read a task" in {
+      val expected = Task(examplePizza, 1, 6)
+      val task = Task("example.in")
+      task should be(expected)
+    }
+
+    "slice example pizza" in {
+      val result = Task("example.in").slice().toString
+      val expected =
+        """3 5 1 6
+          |TTTTT
+          |TMMMT
+          |TTTTT
+          |"""
+          .stripMargin
+      result should be(
+        expected)
+    }
   }
 
-  "Hashcode" should "read a task" in {
-    val expected = Task(Seq(
-      Seq('T', 'T', 'T', 'T', 'T'),
-      Seq('T', 'M', 'M', 'M', 'T'),
-      Seq('T', 'T', 'T', 'T', 'T')), 1, 6)
-    val task = readPizza("example.in")
-    task should be(expected)
-  }
+  "Pizza" should {
+    "cut a slice" in {
 
-  "Hashcode" should "slice example pizza" in {
-    val result = slicePizza("example.in")
-    val expected =
-      """3 5 1 6
-        |TTTTT
-        |TMMMT
-        |TTTTT
-        |""".stripMargin
-    result should be(expected)
+    }
   }
-
 }
