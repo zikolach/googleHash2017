bd8f682f0b567a88384314617845f2fb8440bcb7
Nikolay Kushin <zikolach@Nikolays-MacBook-Pro-2.local>
2017-02-23 20:24:23 +0100

    cleanup

---
+++ first round/Videos/src/test/scala/GoogleHashSpec.scala
@@ -0,0 +1,11 @@
+
+import org.scalatest.{Matchers, WordSpec}
+
+
+class GoogleHashSpec extends WordSpec with Matchers {
+  "GoogleHash" should {
+    "test" in {
+      1 should be(1)
+    }
+  }
+}
