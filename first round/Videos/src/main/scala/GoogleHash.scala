bdcbc239142270dff9db1a12d93586da3b1468e2
Radim Pavlicek <radim.pavlicek@gmail.com>
2017-02-23 19:46:46 +0100

    updates

--- first round/Videos/src/main/scala/GoogleHash.scala
+++ first round/Videos/src/main/scala/GoogleHash.scala
@@ -9,19 +9,26 @@ object GoogleHash {
 
   def main(args: Array[String]): Unit = {
     //        val filename = "logo"
-    val filename = "me_at_the_zoo.in"
+//    val filename = "me_at_the_zoo.in"
+    val filename = "example.in"
     val tmp = Source.fromFile(Paths.get(s"$filename").toFile).getLines().toList
 
     val data = tmp.head.split(' ')
-    val videos = Integer.parseInt(data(0))
-    val endpoints = Integer.parseInt(data(1))
-    val requests = Integer.parseInt(data(2))
-    val caches = Integer.parseInt(data(3))
+    val nr_vids = Integer.parseInt(data(0))
+    val nr_ends = Integer.parseInt(data(1))
+    val nr_reqs = Integer.parseInt(data(2))
+    val nr_caches = Integer.parseInt(data(3))
     val csize = Integer.parseInt(data(4))
 
-    println("Videos:  " + videos + " Endpoints: " + endpoints)
-    println("Requests:  " + requests + " Caches: " + caches + " " + csize + " MB")
+    println("Videos:  " + nr_vids + " Endpoints: " + nr_ends)
+    println("Requests:  " + nr_reqs + " Caches: " + nr_caches + " " + csize + " MB")
 
+    val next = tmp.tail
+
+    val videos = next.head
+    val vids: Array[Int] = videos.split(" ").filterNot(_.isEmpty).map(a => a.toInt)
+
+    println( vids.mkString(" MB "))
 
 
 //    var i = 0
