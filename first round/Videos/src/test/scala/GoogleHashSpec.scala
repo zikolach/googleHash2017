99f4126a03158af73b8a2e01987e6927b9dcf057
Nikolay Kushin <zikolach@Nikolays-MacBook-Pro-2.local>
2017-02-23 21:10:28 +0100

    1 test

--- first round/Videos/src/test/scala/GoogleHashSpec.scala
+++ first round/Videos/src/test/scala/GoogleHashSpec.scala
@@ -3,9 +3,12 @@ import org.scalatest.{Matchers, WordSpec}
 
 
 class GoogleHashSpec extends WordSpec with Matchers {
+  import GoogleHash._
+
   "GoogleHash" should {
-    "test" in {
-      1 should be(1)
+    "read a task" in {
+      val task = readTask("example.in")
+      task.endpoints.length shouldBe 2
     }
   }
 }
