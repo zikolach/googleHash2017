4215332a0f0b5dd8d73713163c0f994d49b24447
Radim Pavlicek <radim.pavlicek@gmail.com>
2017-02-23 19:07:07 +0100

    init commit

---
+++ first round/Videos/src/main/scala/GoogleHash.scala
@@ -0,0 +1,34 @@
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object GoogleHash {
+
+
+  def main(args: Array[String]): Unit = {
+    //        val filename = "logo"
+    val filename = "example"
+    //    val filename = "learn_and_teach"
+    val tmp = Source.fromFile(Paths.get(s"$filename.in").toFile).getLines().toList
+
+    val data = tmp.head.split(' ')
+    val rows = Integer.parseInt(data(0))
+    val cols = Integer.parseInt(data(1))
+    val min = Integer.parseInt(data(2))
+    val max = Integer.parseInt(data(3))
+
+    println("Rows " + rows + " Cols: " + cols)
+
+
+
+//    var i = 0
+//    val commands = tmp.tail.zipWithIndex.flatMap {
+//      case (line, index) => p1(index, line)
+//    }
+//    commands.foreach(println)
+//
+//    Files.write(Paths.get(s"$filename.out"), (commands.length.toString :: commands).mkString("\n").getBytes(StandardCharsets.UTF_8))
+  }
+}
